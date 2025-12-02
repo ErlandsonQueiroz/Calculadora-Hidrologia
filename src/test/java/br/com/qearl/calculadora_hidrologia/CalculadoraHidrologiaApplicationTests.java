79aac3e58278234c18f9942e2522aa514ff10467
Erlandson Queiroz <erlandsonqueiroz@gmail.com>
2025-12-02 20:58:19 -0300

    spring initializr

---
+++ src/test/java/br/com/qearl/calculadora_hidrologia/CalculadoraHidrologiaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.qearl.calculadora_hidrologia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraHidrologiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
